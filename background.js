39b2213238229112237357aa0fa436e8f0c305da
tgg <gatheruteving@gmail.com>
2024-05-30 13:49:41 +0300

    split to background script

--- background.js
+++ background.js
@@ -1,41 +1,38 @@
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//   console.log("Received message:", request);
-//   if (request.file) {
-//     var file = request.file;
-//     var allowedExtensions = /(\.pdf)$/i;
-//     if (!allowedExtensions.exec(file.name)) {
-//       alert("Invalid file type");
-//       sendResponse({ success: false, message: "Invalid file type" });
-//       return false;
-//     }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log("Received message:", request);
+  if (request.file) {
+    var file = request.file;
+    var allowedExtensions = /\.pdf$/i;
+    console.log("Received file:", file);
+    if (!allowedExtensions.exec(request.fileName)) {
+      console.log("Invalid file type", request);
+      alert("Invalid file type");
+      sendResponse({ success: false, message: "Invalid file type" });
+      return false;
+    }
 
-//     processFile(file)
-//       .then(() => {
-//         sendResponse({ success: true, message: "File processed successfully" });
-//       })
-//       .catch((error) => {
-//         sendResponse({ success: false, message: error.message });
-//       });
+    processFile(file)
+      .then(() => {
+        sendResponse({ success: true, message: "File processed successfully" });
+      })
+      .catch((error) => {
+        sendResponse({ success: false, message: error.message });
+      });
 
-//     return true;
-//   }
-// });
+    return true;
+  } else {
+    sendResponse({ success: false, message: "Bad request." });
+  }
+});
 
-// function processFile(file) {
-//   return new Promise((resolve, reject) => {
-//     try {
-//       console.log(`Processing file: ${file.name}`);
-//       resolve();
-//     } catch (error) {
-//       reject(error);
-//     }
-//   });
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("Message received in background script:", request);
-    sendResponse({ response: "Message received" });
+function processFile(file) {
+  return new Promise((resolve, reject) => {
+    try {
+      console.log(`Processing file: ${file.name}`);
+      resolve();
+    } catch (error) {
+      reject(error);
+    }
   });
-// });
+}
 
--- content.js
+++ content.js
@@ -2,12 +2,13 @@ document.getElementById("uploadButton").addEventListener("click", function () {
   const fileUpload = document.getElementById("fileUpload");
   const file = fileUpload.files[0];
 
+  console.log("file: ", file);
   if (!file) {
     alert("Please select a file");
     return;
   }
-
-  if (!/\.pdf$/.test(file.name)) {
+  const pdfExtention = /\.pdf$/i;
+  if (!pdfExtention.exec(file.name)) {
     alert("Invalid file type. Please select a PDF");
     return;
   }
@@ -16,35 +17,10 @@ document.getElementById("uploadButton").addEventListener("click", function () {
     fileName: file.name,
     fileSize: file.size,
     fileType: file.type,
+    file: file,
   };
+
   var myExtensionId = chrome.runtime.id;
-  
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    console.log("message:", message, "Tab: ", tabs[0].id);
-    chrome.tabs.sendMessage(tabs[0].id, message, null, function (response) {
-      try {
-        if (chrome.runtime.lastError) {
-          console.error("tabs: ", chrome.runtime.lastError.message);
-        } else {
-          console.log("Received response:", response);
-        }
-      } catch (error) {
-        console.error("tabs error: ", error);
-      }
-    });
-    console.log("message:", message, "Extention: ", myExtensionId);
-    chrome.runtime.sendMessage(myExtensionId, message, function (response) {
-      try {
-        if (chrome.runtime.lastError) {
-          console.error("runtime: ", chrome.runtime.lastError.message);
-        } else {
-          console.log("Received response:", response);
-        }
-      } catch (error) {
-        console.error("runtime error: ", error);
-      }
-    });
-  });
 
   console.log("message:", message, "Extention: ", myExtensionId);
   chrome.runtime.sendMessage(myExtensionId, message, function (response) {
